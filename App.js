4a3acd5981afd266cf96d0b432e34ff320bac4d4
Vishal Venkatesh Sortur <119862032+vishalsortur2@users.noreply.github.com>
2024-03-18 22:33:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import WeatherApp from './Components/Weather App/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
